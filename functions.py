0ccbeb96ebb5320acce60af44ea60d48bebf9336
abdelaziz <abdelaziz.59140@gmail.com>
2019-11-20 11:39:34 +0100

    add functions UserChoice and ControlChoice

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+Choosen = ["PIERRE", "FEUILLE", "CISEAUX"]
+
 def UserName():
     """functions ask UserName"""
     user = input("quel est ton nom ? ")
@@ -11,4 +13,22 @@ def ControlName(user):
     try:
         assert len(user) > 2 
     except AssertionError as a:
-        return False 
+        return False
+
+def Userchoice():
+    """user choice value"""
+    print("Choisissez entre PIERRE, FEUILLE, CISEAUX")
+    PlayerChoice = input("Votre choix est ").upper()
+    while ControlChoice(PlayerChoice) is False:
+        print("Choisissez entre PIERRE, FEUILLE, CISEAUX")
+        PlayerChoice = input("Votre choix est ").upper()
+    return PlayerChoice
+
+def ControlChoice(PlayerChoice):
+    """function for control choice"""
+    try:
+        assert PlayerChoice in Choosen
+    except AssertionError as a:
+        return False
+
+
--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from functions import *
 
 PlayerName = UserName()
 print(PlayerName)
+
+User1 = Userchoice()
+print(User1)
